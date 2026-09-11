3f28a0de1fa7e3dba2dd7b0fa721706b71017739
marcfunosasvila-design <marcfunosasvila@gmail.com>
2026-09-11 19:01:35 +0200

    Refresh PWA cache for flow-adjusted Performance labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="jarvis-control-center-v26";
+const CACHE="jarvis-control-center-v27";
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./icon.svg"])))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
